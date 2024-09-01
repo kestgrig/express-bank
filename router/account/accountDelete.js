f89dd9abfa90e1617eccd78034bb556435869e40
kestgrig <kestutis.grigenas@gmail.com>
2024-09-01 09:01:53 +0300

    deleteAccUp

--- router/account/accountDelete.js
+++ router/account/accountDelete.js
@@ -1,41 +1,36 @@
 import { accounts } from '../../data/accountsData';
 
 accountDelete.delete('/api/account/:name', (req, res) => {
-    const { name } = req.params; //istraukiamas name 
-    const index = accountIndex(name); //grazina saskaitos indeksa
-    const account = accounts[index];  //objektas iš accounts masyvo pagal index.
+    const { name } = req.params; //istraukiamas name
+    const index = accounts.findIndex(account =>
+        account.name.toLowerCase() === name.toLowerCase());     // surasti saskaitos index pagal varda
+
+    //patikrinti ar tokia saskaita egzistuoja, jei ne ismesti klaida su zinute
+
+    if (index === -1) {
+        return res.json({
+            status: "error",
+            message: "Tokia sąskaita nerasta."
+        });
+    }
 
     //patikrinti ar money didesnis uz 0, jei likutis !0 negalim istrinti.
-    //patikrinti ar tokia saskaita egzistuoja, jei ne ismesti klaida su zinute
+
+    const account = accounts[index];
 
     if (account.money > 0) {
         return res.json({
             status: "error",
             message: "Sąskaita negali būti ištrinta, sąskaitos likutis turi būti 0."
         });
-    } else if (index === -1) {
-        return res.json({
-            status: "error",
-            message: "Tokia sąskaita nerasta."
-        });
 
     }
 
-    // Delete the account if balance is 0
+    //jei saskaitoje nera pinigu ir ji egzistuoja istrinti accounta
+
     accounts.splice(index, 1);
     return res.json({
-        status: "sėkmė",
+        status: "success",
         message: "Sąskaita sėkmingai ištrinta."
     });
 });
-
-const index = getAccountIndex(req.params.name)
-if (index === -1) {
-    return res.json({ status: "error", message: "No account with that name" });
-} else if (accounts[index].money === 0) {
-    accounts.splice(index, 1);
-    return res.json({ status: "success", message: "Account deleted" });
-} else if (accounts[index].money > 0) {
-    return res.json({ status: "error", message: "Account balance needs to be 0" })
-}
-})
