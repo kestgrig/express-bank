3cd221d5091ce86bd30f0e9ee19a378e076f3a86
kestgrig <kestutis.grigenas@gmail.com>
2024-09-02 03:31:33 +0300

    index

--- index.js
+++ index.js
@@ -3,16 +3,32 @@ import express from 'express';
 const app = express();
 const port = 5100;
 
-app.get('/', (req, res) => {
-    return res.send('Laba diena, Lietuva!');
+app.use(express.json({
+    type: 'application/json',
+}));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.use('/', routerMain);
+
+
+app.use((req, res, next) => {
+    return res.status(404).send("Ups... Norimas puslapis nerastas");
 });
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    return res.status(500).send('Ups... Serverio klaida 500');
+});
+
+
 app.get('*', (req, res) => {
-    return res.send('Ups... Norimas puslapis nerastas');
+    return res.send('');
 });
 
 app.listen(port, () => {
-    console.log(`App running on: http://localhost:${port}`);
+    console.log(`Serveris pasileido:: http://localhost:${port}`);
 })
 
 
